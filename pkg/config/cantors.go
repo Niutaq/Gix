c29b6dc495827349449621b9bfae05350a289f3e
Maciej Karczmarz <maciekk1305@gmail.com>
2025-11-08 18:45:36 +0100

    Formatting changes to main(s).go, resolved golangci-lint errors.

--- pkg/config/cantors.go
+++ pkg/config/cantors.go
@@ -3,7 +3,6 @@ package config
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 	"time"
 )
 
@@ -41,28 +40,4 @@ type CantorConfig struct {
 	URL                 string   `json:"url"`
 	DefaultTimeout      Duration `json:"defaultTimeout"`
 	NeedsRateFormatting bool     `json:"needsRateFormatting"`
-}
-
-// LoadCantorConfig reads a JSON file and unmarshal it into a slice of CantorConfig.
-func LoadCantorConfig(filePath string) ([]CantorConfig, error) {
-	fileData, err := os.ReadFile(filePath)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to read cantor config file %s: %w", filePath, err)
-	}
-
-	var configs []CantorConfig
-	if err := json.Unmarshal(fileData, &configs); err != nil {
-		return nil, fmt.Errorf("Failed to unmarshal cantor config from %s: %w", filePath, err)
-	}
-
-	return configs, nil
-}
-
-// LoadCantorConfigFromBytes unmarshalls byte data into a slice of CantorConfig.
-func LoadCantorConfigFromBytes(data []byte) ([]CantorConfig, error) {
-	var configs []CantorConfig
-	if err := json.Unmarshal(data, &configs); err != nil {
-		return nil, fmt.Errorf("Failed to unmarshal cantor config from bytes: %w", err)
-	}
-	return configs, nil
 }
